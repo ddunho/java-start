0de5ae4ccf2946d31cec3ccbf76741967d6fad55
Jaehyuk Choi <dydkwogur@naver.com>
2024-05-04 22:24:08 +0900

    first commit

---
+++ src/variable/Var2.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var2 {
+    public static void main(String[] args) {
+        int a; //변수 선언(그릇과 같은 개념)
+        a = 10; //변수를 10으로 초기화(그릇안에 집어넣는개념)
+
+        System.out.println(a);
+        System.out.println(a); //sout : System.out.println()
+        System.out.println(a);
+    }
+}
