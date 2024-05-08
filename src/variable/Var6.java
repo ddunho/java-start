6f39ece7d67fe08766f40e5967657510b5ae6d99
Jaehyuk Choi <dydkwogur@naver.com>
2024-05-08 12:44:59 +0900

    fisrt commit

---
+++ src/variable/Var6.java
@@ -0,0 +1,10 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        int a;
+        System.out.println(a);
+        //변수를 초기화 하지 않고 출력하면, 오류가 뜬다(컴파일 오류)
+        //지역 변수는 선언한 후에, 초기화도 해줘야함
+    }
+}
