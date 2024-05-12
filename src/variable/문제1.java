9ca29f706560928b21c4d3542d38a37e1e234a6c
Jaehyuk Choi <dydkwogur@naver.com>
2024-05-12 11:06:36 +0900

    first commit

---
+++ src/variable/문제1.java
@@ -0,0 +1,17 @@
+//다음코드를 반복해서 나오는 숫자 4,3을 다른 숫자로 한번에 변경할 수 있도록 다음을 변수 num1,num2를 사용하도록 변형해보아라
+
+package variable;
+
+import java.sql.SQLOutput;
+
+public class 문제1 {
+    public static void main(String[] args) {
+        int num1;
+        int num2; //int num = 4, int num = 3도 가능
+        num1 = 4;
+        num2 = 3;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+    }
+}
