185e39b57cd971c57c417e4821f528c85bddc01b
Jaehyuk Choi <dydkwogur@naver.com>
2024-05-13 17:37:13 +0900

    first commit

---
+++ src/variable/문제2.java
@@ -0,0 +1,12 @@
+//변수 num1, num2를 선언하고, 각각 10, 20을 할당하시오.
+//두 변수의 합을 구하고, 그 결과를 새로운 변수 sum에 저장한 후 sum 변수를 출력하시오
+package variable;
+
+public class 문제2 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+        System.out.println(sum);
+    }
+}
