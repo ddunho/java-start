d97912fc75eca2fa72d1f1a3a09b15b31f04274d
Jaehyuk Choi <dydkwogur@naver.com>
2024-05-15 22:12:02 +0900

    first commit

---
+++ src/operator/Operator2.java
@@ -0,0 +1,32 @@
+package operator;
+
+public class Operator2 {
+    public static void main(String[] args) {
+
+        //문자열과 문자열 더하기1
+        String result1 = "hello " + "world";
+        System.out.println(result1);
+        //hello world
+
+        //문자열과 문자열 더하기2
+        String s1 = "string1";
+        String s2 = "string2";
+        String result2 = s1 + s2;
+        System.out.println(result2);
+        //string1string2
+
+        //문자열과 숫자 더하기1
+        String result3 = "a + b = " + 10; //Java가 문자열로 인식하고 문자여롤 더함
+        System.out.println(result3);
+        // a + b = 10
+
+        //문자열과 숫자 더하기2
+        int num = 20;
+        String str = "a + b = ";
+        String result4 = "a + b = " + num;
+        System.out.println(result4);
+        // a + b = 20
+
+    }
+
+}
