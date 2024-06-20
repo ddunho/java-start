ab2b96a3356bbc77cd18ad6438e0c2e96cea74b6
Jaehyuk Choi <dydkwogur@naver.com>
2024-06-21 08:32:16 +0900

    fisrt commit

---
+++ src/loop/Whilte1_1.java
@@ -0,0 +1,14 @@
+package loop;
+
+public class Whilte1_1 {
+    public static void main(String[] args) {
+        int count = 0;
+
+        count = count + 1;
+        System.out.println("현재 숫자는:" + count);
+        count = count + 1;
+        System.out.println("현재 숫자는:" + count);
+        count = count + 1;
+        System.out.println("현재 숫자는:" + count); //매번 이렇게 계속 복붙할 수는 없다.
+    }
+}
