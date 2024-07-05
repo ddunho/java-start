ee44f5403cb08636b5f565c28a7f27b432d1eeb1
Jaehyuk Choi <dydkwogur@naver.com>
2024-07-05 20:52:09 +0900

    first commit

--- src/loop/Break3.java
+++ src/loop/Break3.java
@@ -4,7 +4,7 @@ public class Break3 {
     public static void main(String[] args) {
         int sum = 0;
 
-        for(int i = 1; ; i++){//for문은 조건식이 없으면 무한 반복한다.
+        for(int i = 1; ; i++){//for문은 조건식이 없으면 무한 반복한다. for문이 while문보다 깔끔하고 보기좋다.
             sum =sum + i;
             if (sum > 10){
                 System.out.println("합이 10보다 크면 종료: i=" + i + " sum=" + sum);
