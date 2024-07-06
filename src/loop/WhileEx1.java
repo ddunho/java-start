dcd59662f9db80c0daf214f71d9938dd909f05fd
Jaehyuk Choi <dydkwogur@naver.com>
2024-07-07 08:51:22 +0900

    first commit

---
+++ src/loop/WhileEx1.java
@@ -0,0 +1,10 @@
+package loop;
+//처음 10개의 자연수를 출력하는 프로그램을 작성해보시오. count라는 변수를 사용해야 합니다.
+public class WhileEx1 {
+    public static void main(String[] args) {
+        int count;
+        for(count=1; count <= 10; count++){
+            System.out.println(count);
+        }
+    }
+}
