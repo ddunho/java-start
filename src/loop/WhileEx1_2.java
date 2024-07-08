8f392205bc743e7f7a0ce515321e995196a65e0d
Jaehyuk Choi <dydkwogur@naver.com>
2024-07-08 22:09:01 +0900

    first commit

---
+++ src/loop/WhileEx1_2.java
@@ -0,0 +1,11 @@
+package loop;
+
+public class WhileEx1_2 {
+    public static void main(String[] args) {
+        int count = 1;
+        while (count <= 10){
+            System.out.println(count);
+            count++;
+    }
+    }
+}
