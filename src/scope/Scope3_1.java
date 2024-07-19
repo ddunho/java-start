83acdd53ab65ff4c98350400a0832ec4eeafac75
Jaehyuk Choi <dydkwogur@naver.com>
2024-07-20 00:28:19 +0900

    first commit

--- src/scope/Scope3_1.java
+++ src/scope/Scope3_1.java
@@ -9,5 +9,5 @@ public class Scope3_1 {
             System.out.println("temp = " + temp);
         }
         System.out.println("m= " + m); //temp 는 if문에서 사용하기 위함이기때문에 생존범위가 중요(비효율적)
-    }
+    }//(temp를 계속 신경써야함)
 }
