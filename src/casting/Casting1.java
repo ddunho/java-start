d4ec2206cee59f38567e45fc7da945a99b63085f
Jaehyuk Choi <dydkwogur@naver.com>
2024-07-21 22:11:52 +0900

    first commit

---
+++ src/casting/Casting1.java
@@ -0,0 +1,18 @@
+package casting;
+
+public class Casting1 {
+    public static void main(String[] args) {
+        int intValue = 10;
+        long longValue;
+        double doubleValue;
+
+        longValue = intValue; //int -> long (성립)
+        System.out.println("longValue = " + longValue);
+
+        doubleValue = intValue; //int -> double
+        System.out.println("doubleValue = " + doubleValue);
+
+        doubleValue = 20L; //L : long
+        System.out.println("doubleValue = " + doubleValue);
+    }
+}
