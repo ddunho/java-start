1ca2583f01be0f5dc0c3a7df4b312c69407f5cd9
Jaehyuk Choi <dydkwogur@naver.com>
2024-07-23 00:02:11 +0900

    first commit

---
+++ src/casting/Casting2.java
@@ -0,0 +1,13 @@
+package casting;
+
+public class Casting2 {
+    public static void main(String[] args) {
+        double doubleValue = 1.5;
+        int intValue = 0;
+
+        //intValue = doubleValue; 컴파일 오류 : 소수점이 날라가면 안되기때문
+        intValue = (int) doubleValue; //소수점이 날라가도 변환하고싶을때 사용
+        System.out.println(intValue);
+
+    }
+}
