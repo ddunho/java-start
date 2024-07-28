a806bc99e87530832f5f9b3f4d6768b1e4e7e948
Jaehyuk Choi <dydkwogur@naver.com>
2024-07-28 19:25:03 +0900

    fisrt commit

---
+++ src/casting/정리.java
@@ -0,0 +1,12 @@
+package casting;
+
+public class 정리 {
+    public static void main(String[] args) {
+        /*
+        int > long > double
+        작은 범위에서 큰 범위로는 대입할 수 있다 (묵시적 형 변환 또는 자동 형변환이라 한다)
+        큰 범위에서 작은 범위로의 대입은 소수점 버림, 오버플로우와 같은 문제가 발생할 수 있다.
+        같은 타입은 같은 결과를 내며, 서로 다른 타입의 계산은 큰 범위로 자동 형변환이 일어난다.
+         */
+    }
+}
