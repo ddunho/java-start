3942ddb04fc65d5db94ae68b0672c999ab78b65e
Jaehyuk Choi <dydkwogur@naver.com>
2024-08-05 14:17:06 +0900

    first commit

---
+++ src/scanner/ScannerWhile3.java
@@ -0,0 +1,18 @@
+package scanner;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class ScannerWhile3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int sum = 0;
+
+        while (true) {
+            System.out.println("정수를 입력하세요 (0을 입력하면 종료):");
+            int number = input.nextInt();
+
+        }
+    }
+}
