928b369a3df304af27ef589bd759121405768ff6
Jaehyuk Choi <dydkwogur@naver.com>
2024-08-08 20:27:08 +0900

    first commit

---
+++ src/scanner/ScannerEx2.java
@@ -0,0 +1,13 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class ScannerEx2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("하나의 정수를 입력하세요:");
+        int number = scanner.nextInt();
+
+    }
+}
