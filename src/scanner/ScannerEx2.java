00102ee44521730a50ec41b9ba1c0791377abf96
Jaehyuk Choi <dydkwogur@naver.com>
2024-08-09 00:02:58 +0900

    first commit

--- src/scanner/ScannerEx2.java
+++ src/scanner/ScannerEx2.java
@@ -9,5 +9,11 @@ public class ScannerEx2 {
         System.out.print("하나의 정수를 입력하세요:");
         int number = scanner.nextInt();
 
+        if (number % 2 == 0){
+            System.out.println("입력한 숫자 " + number + "는 짝수입니다.");
+        } else {
+            System.out.println("입력한 숫자 " + number + "는 홀수입니다.");
+        }
+
     }
 }
