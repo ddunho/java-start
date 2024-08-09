a955c478bddbd3f63d979c1a1606a71ce356a011
Jaehyuk Choi <dydkwogur@naver.com>
2024-08-10 00:43:43 +0900

    first commit

---
+++ src/scanner/ScannerEx3.java
@@ -0,0 +1,13 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class ScannerEx3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("음식 이름을 입력해주세요: ");
+        String foodName = scanner.nextLine();
+
+    }
+}
