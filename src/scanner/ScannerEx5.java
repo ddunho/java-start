0b51afcd393596ca2d26feda224c13bf66c9b143
Jaehyuk Choi <dydkwogur@naver.com>
2024-08-18 19:48:56 +0900

    second commit

--- src/scanner/ScannerEx5.java
+++ src/scanner/ScannerEx5.java
@@ -12,6 +12,15 @@ public class ScannerEx5 {
         System.out.print("두 번째 숫자를 입력하세요:");
         int num2 = scanner.nextInt();
 
+        //num1이 num2보다 큰 경우, 두 숫자를 교환합니다.
+        if (num1 > num2){
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+        System.out.print("두 숫자 사이의 모든 정수:");
+
+
 
 
     }
