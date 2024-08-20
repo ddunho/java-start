b88817545ea252b4ff1c5961e9101919f26d0618
Jaehyuk Choi <dydkwogur@naver.com>
2024-08-20 21:45:35 +0900

    second commit

--- src/scanner/ScannerEx5.java
+++ src/scanner/ScannerEx5.java
@@ -21,7 +21,8 @@ public class ScannerEx5 {
         System.out.print("두 숫자 사이의 모든 정수:");
         for(int i = num1; i <= num2; i++){
             System.out.print(i);
-            System.out.print(",");d
+            if (i != num2){
+            System.out.print(",");
 
         }
 
