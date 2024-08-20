d4c7a4d1348dbc8ae2471821171394ae526eb8f5
Jaehyuk Choi <dydkwogur@naver.com>
2024-08-21 07:56:30 +0900

    second commit

---
+++ src/scanner/ScannerWhileEx1.java
@@ -0,0 +1,20 @@
+package scanner;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class ScannerWhileEx1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("이름을 입력하세요 (종료를입력하면 종료): ");
+            String name = input.nextLine();
+
+            if (name.equals("종료")) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+        }
+    }
+}
