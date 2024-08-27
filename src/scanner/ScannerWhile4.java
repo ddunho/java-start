717be20a4217282f5c9b22c9aa6ad7d3dcfdd76c
Jaehyuk Choi <dydkwogur@naver.com>
2024-08-28 00:06:07 +0900

    second commit

--- src/scanner/ScannerWhile4.java
+++ src/scanner/ScannerWhile4.java
@@ -11,7 +11,15 @@ public class ScannerWhile4 {
             System.out.println("상품의 가격을 입력하세요 (-1을 입력하면 종료): ");
             int price = input.nextInt();
 
+            if (price == -1){
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+            System.out.print("구매하려는 수량을 입력하세요: ");
+            int quantity = input.nextInt();
 
+            int totalCost = price * quantity;
+            System.out.println("총 비용: " + totalCost);
         }
     }
 }
