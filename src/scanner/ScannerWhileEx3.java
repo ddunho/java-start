78f0239961c562b5b99cc4e3975fba75f0c9fb51
Jaehyuk Choi <dydkwogur@naver.com>
2024-09-02 00:02:23 +0900

    second commit

--- src/scanner/ScannerWhileEx3.java
+++ src/scanner/ScannerWhileEx3.java
@@ -11,5 +11,16 @@ public class ScannerWhileEx3 {
         int input = 0;
 
         System.out.println("숫자를 입력하세요. 입력을 중단하려면 -1을 입력하세요: ");
+        while (true){
+            input = scanner.nextInt();
+            if (input == -1){
+                break;
+            }
+            sum += input;
+            count++;
+        }
+
+        double average = sum / count;
+
     }
 }
