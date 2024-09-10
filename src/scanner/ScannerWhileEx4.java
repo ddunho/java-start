5b4c3f2e5bc2c33522dd60ba447d6faef4a4ce6d
Jaehyuk Choi <dydkwogur@naver.com>
2024-09-11 07:14:47 +0900

    first commit

---
+++ src/scanner/ScannerWhileEx4.java
@@ -0,0 +1,16 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class ScannerWhileEx4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int totalCoast = 0;
+
+        while(true){
+            System.out.println("1: 상품 입력, 2: 결제, 3: 프로그램 종료");
+            int option = input.nextInt();
+
+        }
+    }
+}
