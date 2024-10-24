88d680825fbe0419a764c0766b6ec5653067bfb0
Jaehyuk Choi <dydkwogur@naver.com>
2024-10-24 11:23:14 +0900

    first commit

--- src/array/Array1.java
+++ src/array/Array1.java
@@ -8,6 +8,10 @@ public class Array1 {
         int student4 = 60;
         int student5 = 50;
 
-        
+        System.out.println("학생1 점수: " + student1);
+        System.out.println("학생2 점수: " + student2);
+        System.out.println("학생3 점수: " + student3);
+        System.out.println("학생4 점수: " + student4);
+        System.out.println("학생5 점수: " + student5);
     }
 }
