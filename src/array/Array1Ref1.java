3413ee09dab2ca3f3ea854094c5c5ac7f154f503
Jaehyuk Choi <dydkwogur@naver.com>
2024-10-27 08:13:40 +0900

    first commit

--- src/array/Array1Ref1.java
+++ src/array/Array1Ref1.java
@@ -5,6 +5,11 @@ public class Array1Ref1 {
         int[] students; //배열 변수 선언
         students = new int[5];
 
+        int student1 = 90;
+        int student2 = 80;
+        int student3 = 70;
+        int student4 = 60;
+        int student5 = 50;
 
     }
 }
