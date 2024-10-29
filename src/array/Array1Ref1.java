811997959144481fe794936ef3d93b793715c7d6
Jaehyuk Choi <dydkwogur@naver.com>
2024-10-29 20:23:11 +0900

    first commit

--- src/array/Array1Ref1.java
+++ src/array/Array1Ref1.java
@@ -3,19 +3,20 @@ package array;
 public class Array1Ref1 {
     public static void main(String[] args){
         int[] students; //배열 변수 선언
-        students = new int[5];
+        students = new int[5]; //배열 생성
 
-        int student1 = 90;
-        int student2 = 80;
-        int student3 = 70;
-        int student4 = 60;
-        int student5 = 50;
+        System.out.println(students);
+        students[0] = 90;
+        students[1] = 80;
+        students[2] = 70;
+        students[3] = 60;
+        students[4] = 50;
 
 
-        System.out.println("학생1 점수: " + student1);
-        System.out.println("학생2 점수: " + student2);
-        System.out.println("학생3 점수: " + student3);
-        System.out.println("학생4 점수: " + student4);
-        System.out.println("학생5 점수: " + student5);
+        System.out.println("학생1 점수: " + students[0]);
+        System.out.println("학생2 점수: " + students[1]);
+        System.out.println("학생3 점수: " + students[2]);
+        System.out.println("학생4 점수: " + students[3]);
+        System.out.println("학생5 점수: " + students[4]);
     }
 }
