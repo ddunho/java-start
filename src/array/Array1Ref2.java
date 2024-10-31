040e9c6bf90729520659a657d6c41acd0f4116e4
Jaehyuk Choi <dydkwogur@naver.com>
2024-10-31 13:46:43 +0900

    second commit

--- src/array/Array1Ref2.java
+++ src/array/Array1Ref2.java
@@ -12,7 +12,7 @@ public class Array1Ref2 {
         students[3] = 60;
         students[4] = 50;
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < students.length; i++) {
             System.out.println("학생" + (i + 1) + " 점수: " + students[i]);
         }
         /*System.out.println("학생1 점수: " + students[0]);
