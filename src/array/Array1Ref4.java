e008985672e9b07e267fc1be481c661caec2a801
Jaehyuk Choi <dydkwogur@naver.com>
2024-11-06 01:39:40 +0900

    first commit

---
+++ src/array/Array1Ref4.java
@@ -0,0 +1,17 @@
+package array;
+
+public class Array1Ref4 {
+    public static void main(String[] args) {
+        int[] students = {90, 80, 70, 60, 50}; //배열 생성
+
+
+        for (int i = 0; i < students.length; i++) {
+            System.out.println("학생" + (i + 1) + " 점수: " + students[i]);
+        }
+        /*System.out.println("학생1 점수: " + students[0]);
+        System.out.println("학생2 점수: " + students[1]);
+        System.out.println("학생3 점수: " + students[2]);
+        System.out.println("학생4 점수: " + students[3]);
+        System.out.println("학생5 점수: " + students[4]);*/
+    }
+}
