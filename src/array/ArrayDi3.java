3ae23f17a01c17e664e0bbd387fa944acefecead
Jaehyuk Choi <dydkwogur@naver.com>
2024-12-11 00:47:28 +0900

    first commit

---
+++ src/array/ArrayDi3.java
@@ -0,0 +1,21 @@
+package array;
+
+public class ArrayDi3 {
+
+    public static void main(String[] args){
+
+        int[][] arr = {
+                {1,2,3},
+                {4,5,6}
+        }; //행2, 열3
+
+
+        for (int row = 0; row < 2; row++){
+            for (int column = 0; column < 3; column++){
+                System.out.print(arr[row][column] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
