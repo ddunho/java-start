0fe29ef94017d872df6cccf379a6cff316705183
Jaehyuk Choi <dydkwogur@naver.com>
2024-12-26 02:21:19 +0900

    first commit

--- src/array/EnhancedFor1.java
+++ src/array/EnhancedFor1.java
@@ -3,6 +3,10 @@ package array;
 public class EnhancedFor1 {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
+        //단축키 iter
+        for (int number : numbers) {
+            System.out.println();
+        }
 
         //일반 for문
         for (int i = 0; i < numbers.length; i++){
