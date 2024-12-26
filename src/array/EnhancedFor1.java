3c8aecb0503ae14e51a22b3a7611debd5a63f6cf
Jaehyuk Choi <dydkwogur@naver.com>
2024-12-27 00:54:59 +0900

    first commit

--- src/array/EnhancedFor1.java
+++ src/array/EnhancedFor1.java
@@ -17,5 +17,9 @@ public class EnhancedFor1 {
         for(int number : numbers){
             System.out.println(number);
         }
+        //for-each문을 사용할 수 없는 경우, 증가하는 index 값 필요
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("number" + i + "변의 결과는: " + numbers[i]);
+        }
     }
 }
