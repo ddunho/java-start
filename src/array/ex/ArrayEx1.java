228f797bd798a4a7ba412a08ccfdc784e28c7be6
Jaehyuk Choi <dydkwogur@naver.com>
2024-12-28 17:39:46 +0900

    first commit

---
+++ src/array/ex/ArrayEx1.java
@@ -0,0 +1,9 @@
+package array.ex;
+
+public class ArrayEx1 {
+    public static void main(String[] args) {
+        int[] students = {90, 80, 70, 60, 50};
+
+        int total = students + student2 + student3 + student4 + student5;
+    }
+}
