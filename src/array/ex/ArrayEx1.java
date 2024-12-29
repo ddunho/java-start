936fc6f68c931049a7ae22eefb8d976f277f53aa
Jaehyuk Choi <dydkwogur@naver.com>
2024-12-29 10:08:40 +0900

    first commit

--- src/array/ex/ArrayEx1.java
+++ src/array/ex/ArrayEx1.java
@@ -5,5 +5,9 @@ public class ArrayEx1 {
         int[] students = {90, 80, 70, 60, 50};
 
         int total = students + student2 + student3 + student4 + student5;
+        double average = (double) total / 5;
+
+        System.out.println("점수 총합: " + total);
+        System.out.println("점수 평균: " + average);
     }
 }
