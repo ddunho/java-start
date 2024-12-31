088073761698bc9f178945fbd541b9358f5a9e83
Jaehyuk Choi <dydkwogur@naver.com>
2025-01-01 07:24:04 +0900

    first commit

--- src/array/ex/ArrayEx1.java
+++ src/array/ex/ArrayEx1.java
@@ -2,9 +2,14 @@ package array.ex;
 
 public class ArrayEx1 {
     public static void main(String[] args) {
-        int[] students = {90, 80, 70, 60, 50};
+        int student1 = 90;
+        int student2 = 90;
+        int student3 = 90;
+        int student4 = 90;
+        int student5 = 90;
 
-        int total = students + student2 + student3 + student4 + student5;
+
+        int total = student1 + student2 + student3 + student4 + student5;
         double average = (double) total / 5;
 
         System.out.println("점수 총합: " + total);
---
+++ src/array/ex/ArrayEx1Ref.java
@@ -0,0 +1,7 @@
+package array.ex;
+
+public class ArrayEx1Ref {
+    public static void main(String[] args) {
+        int[] students = {90, 80, 70, 60, 50};
+
+}
