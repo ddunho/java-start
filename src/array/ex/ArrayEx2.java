355566270579580cd91e5c15df0ccfa8e54aa7cf
Jaehyuk Choi <dydkwogur@naver.com>
2025-01-06 15:51:03 +0900

    first commit

---
+++ src/array/ex/ArrayEx2.java
@@ -0,0 +1,13 @@
+package array.ex;
+
+import java.util.Scanner;
+
+public class ArrayEx2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[5];
+
+        System.out.println("5개의 정수를 입력하세요:");
+
+    }
+}
