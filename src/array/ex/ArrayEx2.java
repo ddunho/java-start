72ac45f96a308b3eb3c7c51cd330e9dab1c082e5
Jaehyuk Choi <dydkwogur@naver.com>
2025-01-09 00:56:08 +0900

    first commit

--- src/array/ex/ArrayEx2.java
+++ src/array/ex/ArrayEx2.java
@@ -15,6 +15,9 @@ public class ArrayEx2 {
         System.out.println("출력");
         for (int i = 0; i < numbers.length; i++){
             System.out.print(numbers[i]);
+            if (i < numbers.length-1){
+                System.out.print(", ");
+            }
 
         }
 
