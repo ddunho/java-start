7e05cf9806241471589840c98c0d62772c14e997
Jaehyuk Choi <dydkwogur@naver.com>
2025-01-23 01:57:50 +0900

    first commit

--- src/array/ex/ArrayEx4.java
+++ src/array/ex/ArrayEx4.java
@@ -6,18 +6,18 @@ public class ArrayEx4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] numbers = new int[5];
+        int sum = 0;
+        double average;
 
         System.out.println("5개의 정수를 입력하세요:");
-        for (int i = 0; i < numbers.length; i++){
+        for (int i = 0; i < 5; i++){
             numbers[i] = scanner.nextInt();
+            sum += numbers[i];
         }
 
-        System.out.println("출력");
-        //0,1,2,3,4
-        for (int i = 4; i >= 0; i--){
-            System.out.print(numbers[i]);
-            if (i < numbers.length-1){
-                System.out.print(", ");
+        average = (double) sum / 5;
+
+
             }
 
         }
