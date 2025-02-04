df2798f84e14f2692b46e126e20fd602a0f1e8e5
Jaehyuk Choi <dydkwogur@naver.com>
2025-02-05 01:31:37 +0900

    first commit

---
+++ src/array/ex/ArrayEx6.java
@@ -0,0 +1,14 @@
+package array.ex;
+
+import java.util.Scanner;
+
+public class ArrayEx6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int[] numbers = new int[n];
+
+
+    }
+}
