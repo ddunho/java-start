32608d50dcdbe246716a649071596c991e0e7585
Jaehyuk Choi <dydkwogur@naver.com>
2025-02-12 01:52:05 +0900

    first commit

---
+++ src/array/ex/ArrayEx7.java
@@ -0,0 +1,12 @@
+package array.ex;
+
+import java.util.Scanner;
+
+public class ArrayEx7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] scores = new int[4][3];
+        String[] subjects = {"국어", "영어". "수학"};
+
+    }
+}
