f660788dcf70c710a7c2152603e990aa7095ef67
Jaehyuk Choi <dydkwogur@naver.com>
2025-02-15 11:38:10 +0900

    first commit

--- src/array/ex/ArrayEx7.java
+++ src/array/ex/ArrayEx7.java
@@ -6,7 +6,7 @@ public class ArrayEx7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[][] scores = new int[4][3];
-        String[] subjects = {"국어", "영어". "수학"};
+        String[] subjects = {"국어", "영어", "수학"};
 
         for (int i = 0; i < 4; i++){
             System.out.println((i+1) + "번 학생의 성적을 입력하세요:");
---
+++ src/array/ex/ArrayEx8.java
@@ -0,0 +1,12 @@
+package array.ex;
+
+import java.util.Scanner;
+
+public class ArrayEx8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("학생수");
+            }
+        }
+    }
+}
