a1ee2f6c7ac99de62de8af739466368401124562
Jaehyuk Choi <dydkwogur@naver.com>
2025-02-16 11:48:00 +0900

    first commit

--- src/array/ex/ArrayEx8.java
+++ src/array/ex/ArrayEx8.java
@@ -6,6 +6,8 @@ public class ArrayEx8 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("학생수");
+        int[][] scores = new int[4][3];
+        String[] subjects = {"국어", "영어", "수학"};
             }
         }
     }
