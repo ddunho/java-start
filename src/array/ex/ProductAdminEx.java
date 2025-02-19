aa94257aa43b3a126170b705d9e67bb64564d857
Jaehyuk Choi <dydkwogur@naver.com>
2025-02-20 08:49:52 +0900

    first commit

---
+++ src/array/ex/ProductAdminEx.java
@@ -0,0 +1,10 @@
+package array.ex;
+
+public class ProductAdminEx {
+    public static void main(String[] args) {
+        int maxProducts = 10;
+        String[] productNames = new String[maxProducts];
+        int[] productPrices = new int[maxProducts];
+        int productCount = 0;
+    }
+}
