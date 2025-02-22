e79ed527cd20fcf2191934b68592504fdf2b81d4
Jaehyuk Choi <dydkwogur@naver.com>
2025-02-22 16:25:25 +0900

    first commit

--- src/array/ex/ProductAdminEx.java
+++ src/array/ex/ProductAdminEx.java
@@ -1,10 +1,26 @@
 package array.ex;
 
+import java.util.Scanner;
+
 public class ProductAdminEx {
     public static void main(String[] args) {
         int maxProducts = 10;
         String[] productNames = new String[maxProducts];
         int[] productPrices = new int[maxProducts];
         int productCount = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.print("1. 상품 등록 | 2. 상품 목록 | 3. 종료\n 메뉴를 선택하세요: ");
+            int menu = scanner.nextInt();
+
+            if (menu == 1){
+                System.out.print("상품 이름을 입력하세요:");
+                productNames[productCount] = scanner.nextLine();
+
+                System.out.println("상품 가격을 입력하세요:");
+                productPrices[productCount] = scanner.nextInt();
+            }
+        }
     }
 }
