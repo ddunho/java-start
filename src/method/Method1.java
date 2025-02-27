97386d4684aeb9b9fa168212e92c5a3df10f9ad8
Jaehyuk Choi <dydkwogur@naver.com>
2025-02-28 01:47:42 +0900

    first commit

---
+++ src/method/Method1.java
@@ -0,0 +1,10 @@
+package method;
+
+public class Method1 {
+    public static void main(String[] args) {
+        //계산1
+        int a = 1;
+        int b = 2;
+        System.out.println(a + "+" + b + " 연산 수행");
+    }
+}
