3529dfa4228cac2a66b4a204e3153d4407f30c5d
Jaehyuk Choi <dydkwogur@naver.com>
2025-03-05 09:26:48 +0900

    first commit

--- src/method/Method1Ref.java
+++ src/method/Method1Ref.java
@@ -3,24 +3,17 @@ package method;
 public class Method1Ref {
     public static void main(String[] args) {
         //계산1
-        int a = 1;
-        int b = 2;
-        System.out.println(a + "+" + b + " 연산 수행");
-        int sum1 = a + b;
+        int sum1 = add(5, 10);
         System.out.println("결과1 출력: " + sum1);
+        //계산 2
+        int sum2 = add(15, 20);
+        System.out.println("결과2 출력: " + sum2);
 
-        System.out.println("=================");
-
-        //계산2
-        int x = 10;
-        int y = 20;
-        System.out.println(x + "+" + y + "연산 수행");
-        int sum2 = x + y;
-        System.out.println("결과2 출력:" + sum2);
     }
     //add 매서드
     public static int add(int a, int b){
-        System.out.println(a + "+" + b) + "연산 수행";
+        System.out.println(a + "+" + b + "연산 수행");
         int sum = a + b;
+        return sum;
     }
 }
