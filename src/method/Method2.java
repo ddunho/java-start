77c27f68cd00a16eb50902ba71fce49144a428a9
Jaehyuk Choi <dydkwogur@naver.com>
2025-03-06 22:02:39 +0900

    first commit

---
+++ src/method/Method2.java
@@ -0,0 +1,13 @@
+package method;
+
+public class Method2 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void printHeader(){
+        System.out.println("= 프로그램을 시작합니다 =");
+        return;
+    }
+}
