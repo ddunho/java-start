b77f7385de34b54df6debbb1fb5b1594288eafc9
Jaehyuk Choi <dydkwogur@naver.com>
2025-03-12 09:28:31 +0900

    first commit

---
+++ src/method/MethodReturn1.java
@@ -0,0 +1,14 @@
+package method;
+
+public class MethodReturn1 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean odd(int i){
+        if( i % 2 == 1){
+            return true;
+        }
+    }
+}
