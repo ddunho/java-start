b3c22199aafe9daebc23ca7af0e599372eccc964
Jaehyuk Choi <dydkwogur@naver.com>
2025-03-15 00:34:28 +0900

    first commit

---
+++ src/method/MethodReturn2.java
@@ -0,0 +1,14 @@
+package method;
+
+public class MethodReturn2 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void checkAge(int age){
+        if(age < 18){
+
+        }
+    }
+}
