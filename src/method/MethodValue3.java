2e76cba450fdb2eea0aa356d75be2ff00d7eb31f
Jaehyuk Choi <dydkwogur@naver.com>
2025-04-02 01:57:55 +0900

    first commit

---
+++ src/method/MethodValue3.java
@@ -0,0 +1,9 @@
+package method;
+
+public class MethodValue3 {
+
+    public static void main(String[] args) {
+        int num1 = 5;
+        System.out.println("1. changeNumber 호출 전, num1: " + num1);
+    }
+}
