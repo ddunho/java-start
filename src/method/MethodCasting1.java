d73c185823ef6b034117a6594c996e033eaa4851
Jaehyuk Choi <dydkwogur@naver.com>
2025-04-08 21:03:23 +0900

    rist commit

---
+++ src/method/MethodCasting1.java
@@ -0,0 +1,8 @@
+package method;
+
+public class MethodCasting1 {
+    public static void main(String[] args) {
+        double number = 1.5;
+        //printNumber(number); // double을 int 형에 대입하므로 컴파일 오류
+    }
+}
--- src/method/MethodValue3.java
+++ src/method/MethodValue3.java
@@ -4,9 +4,9 @@ public class MethodValue3 {
 
     public static void main(String[] args) {
         int num1 = 5;
-        System.out.println("1. changeNumber 호출 전, num1: " + num1);
-        num1 = changeNumber(num1);
-        System.out.println("4. changeNumber 호출 전, num1: " + num1);
+        System.out.println("changeNumber 호출 전, num1: " + num1);
+        num1 = 10;
+        System.out.println("changeNumber 호출 전, num1: " + num1);
 
     }
     public static int changeNubmer(int num2){
