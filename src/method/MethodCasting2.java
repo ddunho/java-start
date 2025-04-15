5fef69e6397726e13afa2a2b64bc6a5761bc4baf
Jaehyuk Choi <dydkwogur@naver.com>
2025-04-15 22:19:58 +0900

    first commit

--- src/method/MethodCasting2.java
+++ src/method/MethodCasting2.java
@@ -3,6 +3,7 @@ package method;
 public class MethodCasting2 {
     public static void main(String[] args) {
         int number = 100;
+        printNumber(number); //int에서 double로 자동 형변환
     }
 
     public static void printNumber(double n){
