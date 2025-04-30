7366ded61c1d7da2a078e260756f13ac4a9b3cfe
Jaehyuk Choi <dydkwogur@naver.com>
2025-04-30 22:18:03 +0900

    first commit

--- src/overloading/Overloading1.java
+++ src/overloading/Overloading1.java
@@ -3,6 +3,8 @@ package overloading;
 public class Overloading1 {
 
     public static void main(String[] args) {
+        System.out.println("1: " + add(1,2));
+        System.out.println("2: " + add(1, 2, 3));
 
     }
     public static int add(int a, int b){
