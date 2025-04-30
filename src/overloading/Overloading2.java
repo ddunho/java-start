7e0f0e594477e89a0f4543c999505fe6c56846d7
Jaehyuk Choi <dydkwogur@naver.com>
2025-05-01 06:27:49 +0900

    first commit

---
+++ src/overloading/Overloading2.java
@@ -0,0 +1,12 @@
+package overloading;
+
+public class Overloading2 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void myMethod(int a, double b){
+        System.out.println("int a, double b");
+    }
+}
