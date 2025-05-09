01f1bef6efb77a271845dceba40bc2afe6b71bab
Jaehyuk Choi <dydkwogur@naver.com>
2025-05-09 18:43:00 +0900

    first commit

---
+++ src/overloading/Overloading3.java
@@ -0,0 +1,12 @@
+package overloading;
+
+public class Overloading3 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int add(int a, int b){
+        System.out.println("1번 호출");
+    }
+}
