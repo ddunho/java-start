f2a157ca4eb3dc70a3512c4d6a5b680dfeb68800
Jaehyuk Choi <dydkwogur@naver.com>
2025-05-29 17:49:05 +0900

    first commit

--- src/method/ex/MethodEx1.java
+++ src/method/ex/MethodEx1.java
@@ -19,5 +19,9 @@ public class MethodEx1 {
 
 public static double average(int a, int b, int c){
         int sum = a +  b + c;
+        double average = sum / 3.0;
+    System.out.println("평균값: " + average);
+
+
     }
 }
