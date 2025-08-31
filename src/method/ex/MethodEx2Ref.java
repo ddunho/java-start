7ea20a54cc3cb0dc304145fb603f57b5be67a940
Jaehyuk Choi <dydkwogur@naver.com>
2025-08-31 11:33:01 +0900

    first commit

--- src/method/ex/MethodEx2Ref.java
+++ src/method/ex/MethodEx2Ref.java
@@ -2,7 +2,8 @@ package method.ex;
 
 public class MethodEx2Ref {
     public static void main(String[] args) {
-        String message = "Hello, world!";
+        String message = "Hello, world!";for(int i = 0; i < 3; i++){
+            System.out.println(message);
 
     }
 
